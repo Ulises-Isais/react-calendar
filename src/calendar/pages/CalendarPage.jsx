cf4483ed81e5fdc74a0c299b386a99bcdbcaec69
Ulises <ulises_isais@hotmail.com>
2023-08-03 13:28:43 -0600

    Subiendo Archivos

---
+++ src/calendar/pages/CalendarPage.jsx
@@ -0,0 +1,67 @@
+import { Calendar } from "react-big-calendar";
+import "react-big-calendar/lib/css/react-big-calendar.css";
+import { addHours } from "date-fns";
+
+import { CalendarEvent, Navbar } from "../";
+import { localizer, getMessagesES } from "../../helpers";
+import { useState } from "react";
+
+const events = [
+  {
+    title: "Cumpleaños del jefe",
+    notes: "Comprar pastel",
+    start: new Date(),
+    end: addHours(new Date(), 2),
+    bgColor: "#fafafa",
+    user: {
+      _id: "123",
+      name: "Ulises",
+    },
+  },
+];
+
+export const CalendarPage = () => {
+  const [lastView, setLastView] = useState(
+    localStorage.getItem("lastView") || "week"
+  );
+  const eventStyleGetter = (event, start, ende, isSelected) => {
+    // console.log({ event, start, ende, isSelected });
+    const style = {
+      backgroundColor: "#347CF7",
+      borderRadius: "0px",
+      opacity: 0.8,
+      color: "white",
+    };
+    return {
+      style,
+    };
+  };
+
+  const onDoubleClick = (event) => {};
+  const onSelect = (event) => {};
+  const onViewChanged = (event) => {
+    localStorage.setItem("lastView", event);
+    setLastView(event);
+  };
+  return (
+    <>
+      <Navbar />
+
+      <Calendar
+        culture="es"
+        localizer={localizer}
+        events={events}
+        defaultView={lastView}
+        startAccessor="start"
+        endAccessor="end"
+        style={{ height: 500 }}
+        messages={getMessagesES()}
+        eventPropGetter={eventStyleGetter}
+        components={{ event: CalendarEvent }}
+        onDoubleClickEvent={onDoubleClick}
+        onSelectEvent={onSelect}
+        onView={onViewChanged}
+      />
+    </>
+  );
+};
